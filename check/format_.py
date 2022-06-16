e12e09ebff5ec2225db53abfe878c857af0e6e02
Michael A. Perlin <mika.perlin@gmail.com>
2022-06-16 16:09:27 -0500

    eliminate [root_dir]/check/*.py in favor of symlinking check to applications_superstaq/check

--- check/format_.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-import applications_superstaq
-
-if __name__ == "__main__":
-    exit(applications_superstaq.check.format_.run(*sys.argv[1:]))
--- check/mypy_.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-from applications_superstaq import check
-
-if __name__ == "__main__":
-    exit(check.mypy_.run(*sys.argv[1:]))
