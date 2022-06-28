3f5e41dc9e80fd4b310a0140db8139abedaa8405
Michael A. Perlin <mika.perlin@gmail.com>
2022-06-27 20:17:39 -0500

    testing a relative import...

--- applications_superstaq/check/format_.py
+++ applications_superstaq/check/format_.py
@@ -7,7 +7,7 @@ import sys
 import textwrap
 from typing import Iterable
 
-from applications_superstaq.check import check_utils
+from check import check_utils
 
 default_files_to_check = ("*.py", "*.ipynb")
 
